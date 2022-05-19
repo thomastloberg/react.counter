2e8643014cd7641996c3ffd6f0427ab0ea506cba
Thomas T. Løberg <thtech93@gmail.com>
2022-05-19 22:10:00 +0200

    Completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
+
+function App(){
+  const[count, setCount] = useState(0);
+
+  return(
+    <div className="app">
+      <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>{count}</h1>
+      <div className="button__wrapper">
+        <button className='negative' onClick={() => setCount(count-1)}>-</button>
+        <button className='positive' onClick={() => setCount(count+1)}>+</button>
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
